1a06fd4db1a10cffbbfdfbc69b8701da9b8db132
Gadzzaa <robert.popescu10@outlook.com>
2022-04-21 12:27:28 +0300

    Project Created

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace GadzzaaTB_2._0
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
