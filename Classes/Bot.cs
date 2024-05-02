ae058a2b61a742862f37d89df3634f0c2d066b9f
gadzzaa <popescurobert986@gmail.com>
2024-05-02 14:14:44 +0300

    a

--- Classes/Bot.cs
+++ Classes/Bot.cs
@@ -15,9 +15,9 @@ namespace GadzzaaTB.Classes;
 
 public class Bot
 {
-    private readonly MainWindow? _mainWindow = (MainWindow)Application.Current.MainWindow;
+    private readonly MainWindow _mainWindow = (MainWindow)Application.Current.MainWindow;
     public readonly TwitchClient Client;
-    public string? JoinedChannel;
+    public string JoinedChannel;
 
     public Bot()
     {
@@ -34,25 +34,24 @@ public class Bot
         Client.OnReconnected += ClientOnOnReconnected;
     }
 
-    private async Task ClientOnOnReconnected(object? sender, OnConnectedEventArgs onConnectedEventArgs)
+    private async Task ClientOnOnReconnected(object sender, OnConnectedEventArgs onConnectedEventArgs)
     {
         if (JoinedChannel != null) await Client.JoinChannelAsync(JoinedChannel);
     }
 
-    private async Task ClientOnOnDisconnected(object? sender, OnDisconnectedEventArgs e)
+    private async Task ClientOnOnDisconnected(object sender, OnDisconnectedEventArgs e)
     {
-        if (_mainWindow != null && _mainWindow.BugReport.IsClosing) return;
         await Client.ReconnectAsync();
         Thread.Sleep(5000);
     }
 
-    private Task Client_OnConnected(object? sender, OnConnectedEventArgs onConnectedEventArgs)
+    private Task Client_OnConnected(object sender, OnConnectedEventArgs onConnectedEventArgs)
     {
         Console.WriteLine(@"Connected to Twitch!");
         return Task.CompletedTask;
     }
 
-    private async Task Client_OnJoinedChannel(object? sender, OnJoinedChannelArgs e)
+    private async Task Client_OnJoinedChannel(object sender, OnJoinedChannelArgs e)
     {
         JoinedChannel = e.Channel;
         if (!Settings.Default.Verified) return;
@@ -60,69 +59,72 @@ public class Bot
         await Client.SendMessageAsync(e.Channel, "Bot online!");
         await Task.Factory.StartNew(() =>
         {
-            if (_mainWindow != null)
+            var _ = _mainWindow.Dispatcher.BeginInvoke((Action)(() =>
             {
-                var _ = _mainWindow.Dispatcher.BeginInvoke((Action)(() =>
                 {
-                    {
-                        _mainWindow.TwitchStatus = "Connected";
-                        _mainWindow.TwitchConnect = "Disconnect";
-                        _mainWindow.ChannelNameBox.IsEnabled = false;
-                    }
-                }));
-            }
+                    _mainWindow.TwitchStatus = "Connected";
+                    _mainWindow.TwitchConnect = "Disconnect";
+                    _mainWindow.ChannelNameBox.IsEnabled = false;
+                }
+            }));
         });
     }
 
-    private Task ClientOnOnLeftChannel(object? sender, OnLeftChannelArgs e)
+    private Task ClientOnOnLeftChannel(object sender, OnLeftChannelArgs e)
     {
         Console.WriteLine(@"Left channel: " + e.Channel);
         JoinedChannel = null;
         Task.Factory.StartNew(() =>
         {
-            if (_mainWindow != null)
+            var _ = _mainWindow.Dispatcher.BeginInvoke((Action)(() =>
             {
-                var _ = _mainWindow.Dispatcher.BeginInvoke((Action)(() =>
                 {
-                    {
-                        _mainWindow.TwitchStatus = "Disconnected";
-                        _mainWindow.TwitchConnect = "Connect";
-                        _mainWindow.ChannelNameBox.IsEnabled = true;
-                    }
-                }));
-            }
+                    _mainWindow.TwitchStatus = "Disconnected";
+                    _mainWindow.TwitchConnect = "Connect";
+                    _mainWindow.ChannelNameBox.IsEnabled = true;
+                }
+            }));
         });
         return Task.CompletedTask;
     }
 
-    private async Task Client_OnMessageReceived(object? sender, OnMessageReceivedArgs e)
+    private async Task Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
     {
-        if (!Settings.Default.Verified && e.ChatMessage.Message == "!verify" &&
-            e.ChatMessage.Username == e.ChatMessage.Channel)
+        Console.WriteLine(Settings.Default.Verified);
+        Console.WriteLine(e.ChatMessage.Message);
+        Console.WriteLine(e.ChatMessage.Username);
+        Console.WriteLine(e.ChatMessage.Channel);
+        if (!Settings.Default.Verified)
         {
-            Settings.Default.Verified = true;
-            if (_mainWindow != null)
-            {
-                _mainWindow.TwitchStatus = "Connected";
-                _mainWindow.TwitchConnect = "Disconnect";
-            }
-
+            if (e.ChatMessage.Message != "!verify") return;
+            if (e.ChatMessage.Username != e.ChatMessage.Channel) return;
+            Verifica();
             await Client.SendMessageAsync(e.ChatMessage.Channel,
                 "Verification process completed! Thank you for using my bot!");
             return;
         }
 
-        if (_mainWindow != null && e.ChatMessage.Message == "!np" && !_mainWindow._sreader.CanRead)
+        if (e.ChatMessage.Message == "!np")
         {
-            await Client.SendMessageAsync(e.ChatMessage.Channel,
-                @"Process 'osu.exe' could not be found running. Please launch the game before using the command");
-            return;
-        }
+            if (_mainWindow.DebugOsu == null) return;
+            if (!_mainWindow._sreader.CanRead)
+            {
+                await Client.SendMessageAsync(e.ChatMessage.Channel,
+                    @"Process 'osu.exe' could not be found running. Please launch the game before using the command");
+                return;
+            }
 
-        if (_mainWindow != null)
             await Client.SendMessageAsync(e.ChatMessage.Channel,
                 _mainWindow.DebugOsu.mStars + "\u2b50 | " + _mainWindow.DebugOsu.mapInfo + " | Mods: " +
                 _mainWindow.DebugOsu.modsText + " | Download: " +
                 _mainWindow.DebugOsu.dl);
+        }
+    }
+
+    private void Verifica()
+    {
+        Settings.Default.Verified = true;
+        _mainWindow.TwitchStatus = "Connected";
+        _mainWindow.TwitchConnect = "Disconnect";
     }
 }
