cc9921cea44f39fa8f1fd079f407d64b41b111bd
gadzzaa <popescurobert986@gmail.com>
2024-05-06 00:33:45 +0300

    Add: App can now start with Windows.

--- Windows/SettingsPage.xaml.cs
+++ Windows/SettingsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
 
 namespace GadzzaaTB.Windows;
 
@@ -7,5 +9,21 @@ public partial class SettingsPage : Page
     public SettingsPage()
     {
         InitializeComponent();
+    }
+
+    private void WindowsStart_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (WindowsStart.IsChecked.Value)
+        {
+            var path = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(path, true);
+            key.SetValue("GadzzaaTB",  System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\GadzzaaTB.exe");
+        }
+        else
+        {
+            var path = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(path, true);
+            key.DeleteValue("GadzzaaTB", false);
+        }
     }
 }
